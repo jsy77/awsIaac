bd80b7534dedc97954995b8934db33c33b696728
jsy <jsy111777@gmail.com>
2026-08-12 12:36:37 +0530

    private ins

--- securitygroup.tf
+++ securitygroup.tf
@@ -3,21 +3,7 @@ resource "aws_security_group" "ec2_sg" {
   vpc_id      = var.vpc_id
   name        = var.sg_name
 
-  ingress {
-    description = "ssh"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["103.81.27.159/32"]
-  }
-
-  ingress {
-    description = "http"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["103.81.27.159/32"]
-  }
+  
 
   egress {
     description = "Allow All"
