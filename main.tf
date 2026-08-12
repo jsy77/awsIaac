17ec93d5f3c4eb205446e610eb8da60a9fa985cd
jsy <jsy111777@gmail.com>
2026-08-12 12:55:26 +0530

    changed sg and ec2 names for tf

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ provider "aws" {
 }
 
 
-resource "aws_instance" "my_ec2" {
+resource "aws_instance" "private_ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  vpc_security_group_ids = [aws_security_group.private_sg]
   
   
   subnet_id              = var.subnet_id
--- securitygroup.tf
+++ securitygroup.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "ec2_sg" {
+resource "aws_security_group" "private_sg" {
   description = "${var.description}-sg"
   vpc_id      = var.vpc_id
   name        = var.sg_name
